412b5e3f826829b7e547e9d802000978911b42fd
vologa <bloshchitsyn@gmail.com>
2019-10-10 23:46:04 +0300

    Convert flat structure to tree example

---
+++ index.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+
+const dataPoints = require('./data_points');
+const testTree = require('./data_tree');
+
+// Sort data points
+dataPoints.sort((a, b) => (a.start + a.tier) > (b.start + b.tier) ? 1 : -1);
+
+// Build tree
+const dataTree = [];
+dataPoints.forEach(p => addNode(p, dataTree));
+
+function addNode(point, tree, previousTier = null) {
+
+    point.children = [];
+
+    if (!tree.length) return tree.push(point)
+
+    for (const node of tree) {
+        if (point.tier.startsWith(node.tier) && previousTier !== node.tier) {
+            return addNode(point, node.children, node.tier);
+        }
+    }
+
+    return tree.push(point);
+}
+
+// Test trees equality
+assert.deepStrictEqual(dataTree, testTree, 'Tree objects are different');
+assert.equal(JSON.stringify(dataTree), JSON.stringify(testTree), 'Serialized tree objects are different');
+
+console.log('Tests are passed');
